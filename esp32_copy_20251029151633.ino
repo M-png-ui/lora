11699e9ee4e31af887f2c35811f3e505736a1c30
Maryam Nosir <132782647+M-png-ui@users.noreply.github.com>
2025-11-03 22:41:45 +0300

    Update esp32_copy_20251029151633.ino

--- esp32_copy_20251029151633.ino
+++ esp32_copy_20251029151633.ino
@@ -2,18 +2,16 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-#define LORA_SS    5   // NSS / CS pin from LoRa -> ESP32 GPIO10
-#define LORA_RST    14   // RESET pin from LoRa -> ESP32 GPIO9
-#define LORA_DIO0  26   // DIO0 pin from LoRa -> ESP32 GPIO25
+#define LORA_SS    5   
+#define LORA_RST    14   
+#define LORA_DIO0  26   
 
 #define LORA_SCK   18   // SCK  -> ESP32 GPIO18
 #define LORA_MISO  19   // MISO -> ESP32 GPIO19
 #define LORA_MOSI  23   // MOSI -> ESP32 GPIO13
 
-#define BUTTON_PIN 27   // زر بين هذا البن و GND
-
-#define LORA_FREQ 433E6 // لو موديولك 868 MHz أو 915 MHz غيريها هنا وهناك
-
+#define BUTTON_PIN 27   
+#define LORA_FREQ 433E6 
 void setup() {
   Serial.begin(115200);
   delay(1000);
