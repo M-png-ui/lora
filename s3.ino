2e238be9a45a8d0169e5cc7899cce5f7ced5b7b3
Maryam Nosir <132782647+M-png-ui@users.noreply.github.com>
2025-11-03 22:43:15 +0300

    Update s3.ino

--- s3.ino
+++ s3.ino
@@ -1,32 +1,26 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-// Pins for LoRa module on ESP32-S3 (Receiver node)
-#define LORA_SS    5   // NSS / CS
-#define LORA_RST    14   // RESET
-#define LORA_DIO0   4   // DIO0 (interrupt)
+#define LORA_SS    5  
+#define LORA_RST    14   
+#define LORA_DIO0   4  
 
-// SPI pins for ESP32-S3 (we will map them manually)
-#define LORA_SCK   18   // SCK
-#define LORA_MISO  19   // MISO
-#define LORA_MOSI  13   // MOSI
+#define LORA_SCK   18   
+#define LORA_MISO  19   
+#define LORA_MOSI  13   
 
-// LED pin (goes to LED via resistor on breadboard)
 #define LED_PIN    21
 
-// same frequency as sender!
 #define LORA_FREQ 433E6
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW); // LED off at start
+  digitalWrite(LED_PIN, LOW); 
 
-  // init SPI for LoRa
   SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI);
 
-  // tell LoRa which control pins
   LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
 
   if (!LoRa.begin(LORA_FREQ)) {
@@ -36,7 +30,6 @@ void setup() {
     }
   }
 
-  // يجب تكون نفس الإعدادات في المرسل لمدى مستقر
   LoRa.setSpreadingFactor(12);
   LoRa.setSignalBandwidth(125E3);
   LoRa.setCodingRate4(5);
@@ -47,7 +40,6 @@ void setup() {
 void loop() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
-    // في باكِت وصلت
     String incoming = "";
 
     while (LoRa.available()) {
@@ -57,11 +49,10 @@ void loop() {
     Serial.print("Received: ");
     Serial.println(incoming);
 
-    // لو الرسالة من العقدة الأولى فيها طلب تشغيل
     if (incoming == "BTN_PRESSED") {
-      digitalWrite(LED_PIN, HIGH); // ولع اللمبة
-      delay(1000);                 // خليك شغّال 1 ثانية
-      digitalWrite(LED_PIN, LOW);  // طفي
+      digitalWrite(LED_PIN, HIGH); 
+      delay(1000);                 
+      digitalWrite(LED_PIN, LOW);  
     }
   }
 }
